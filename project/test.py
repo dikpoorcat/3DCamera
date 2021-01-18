095bbe9fa60f61f8ea2f115ac33ec26ad220d273
子厄 <50310628+dikpoorcat@users.noreply.github.com>
2021-01-18 17:33:53 +0800

    增加高度数据
    增加高度数据，并在图中标出。

--- project/test.py
+++ project/test.py
@@ -21,7 +21,11 @@ def dat2img(filename):
     k = (mean_arry[colum2] - mean_arry[colum1]) / (colum2 - colum1)
     c = np.arange(0, k * 2560, k)
     img = img - c
-    img = img.clip(40)  # 背景：30  饼干：39~43
+    img_height = img.copy()
+    # print(img_height[300, :])
+
+    # 截取40高度位置
+    img = img.clip(40)  # 背景：30  饼干：39~43mm
 
     # 归一化操作，*255当图片显示
     amin, amax = img.min(), img.max()  # 求最大最小值
@@ -43,14 +47,16 @@ def dat2img(filename):
     print('Resized Dimensions : ', resized.shape)
     # plt.imshow(resized)
     # plt.show()
+    img_height = cv2.resize(img_height, dim, interpolation=cv2.INTER_AREA)
 
     # 保存图片
     cv2.imwrite(r'H:\GitHub\3DCamera\docs\photos\resized.png', resized)
+    cv2.imwrite(r'H:\GitHub\3DCamera\docs\photos\img_height.png', img_height)
 
-    return resized
+    return resized, img_height
 
 
-def demopreprocessing(img):
+def demopreprocessing(img, img_height):
     img_original = img.copy()
     img_uint8 = img.astype(np.uint8)  # 转换为整型
     img_info = img.shape
@@ -87,22 +93,22 @@ def demopreprocessing(img):
 
     # 遍历每一个轮廓
     for c in contours:
-        # 找到边界框的坐标
-        x, y, w, h = cv2.boundingRect(c)
-        # 在img图像上 绘制矩形  线条颜色为green 线宽为2
-        #     cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
-
-        # 找到最小区域
-        rect = cv2.minAreaRect(c)
-
-        # 计算最小矩形的坐标
-        box = cv2.boxPoints(rect)
-
-        # 坐标转换为整数
-        box = np.int0(box)
-
-        # 绘制轮廓  最小矩形 blue
-        #     cv2.drawContours(img,[box],0,(255,0,0),3)
+        # # 找到边界框的坐标
+        # x, y, w, h = cv2.boundingRect(c)
+        # # 在img图像上 绘制矩形  线条颜色为green 线宽为2
+        # cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),1)
+        #
+        # # 找到最小区域
+        # rect = cv2.minAreaRect(c)
+        #
+        # # 计算最小矩形的坐标
+        # box = cv2.boxPoints(rect)
+        #
+        # # 坐标转换为整数
+        # box = np.int0(box)
+        #
+        # # 绘制轮廓  最小矩形 blue
+        # cv2.drawContours(img,[box],0,(255,0,0),3)
 
         # 计算闭圆中心点和半径
         (x, y), radius = cv2.minEnclosingCircle(c)
@@ -110,6 +116,7 @@ def demopreprocessing(img):
         # 转换为整型
         center = (int(x), int(y))
         radius = int(radius)
+        # print("中心及半径", center, radius)
 
         # 在mask1绘制闭圆
         mask1 = cv2.circle(mask1, center, radius, (255, 255, 255), 1)
@@ -125,10 +132,23 @@ def demopreprocessing(img):
         # 画出直径
         #     cv2.line(img,(int(x)-radius,int(y)),(int(x)+radius,int(y)),(0,0,255),2)
         # 标出直径
-        pixelsPerMetric = 1
+        pixelsPerMetric = 2560 / 390.683895
         d = radius * 2 / pixelsPerMetric
-        cv2.putText(img, "diameter: {:.1f}pix".format(d),
+        cv2.putText(img, "diameter: {:.2f}mm".format(d),
                     (int(x - 100), int(y - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.65, (50, 100, 255), 2)
+        # 标出高度
+        height = 0
+        k = 10
+        for i in range(k):  # 取k*k范围内像素的平均高度
+            for j in range(k):
+                height += img_height[int(y + i), int(x + j)] - 31.4
+                # print(height)
+        height /= (k * k)
+
+        # height = img_height[int(y), int(x)] - 31.4
+        cv2.putText(img, "height: {:.2f}mm".format(height),
+                    (int(x - 72), int(y - 30)), cv2.FONT_HERSHEY_SIMPLEX,
                     0.65, (50, 100, 255), 2)
 
     # 获取圆内mask（圆内数据有效）
@@ -168,6 +188,6 @@ def demopreprocessing(img):
 
 
 if __name__ == '__main__':
-    img = dat2img(r'H:\GitHub\3DCamera\docs\data\2.dat')
-    demopreprocessing(img)
+    img, img_height = dat2img(r'H:\GitHub\3DCamera\docs\data\0.dat')
+    demopreprocessing(img, img_height)
     print('完成')
